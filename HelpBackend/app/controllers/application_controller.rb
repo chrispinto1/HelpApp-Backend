9322d7807111ab8369cbedfbbc62e850785c36a3
Christian Pinto <pintochristian2@gmail.com>
2019-07-29 15:30:52 -0400

    auth backend

--- HelpBackend/app/controllers/application_controller.rb
+++ HelpBackend/app/controllers/application_controller.rb
@@ -1,23 +1,3 @@
 class ApplicationController < ActionController::API
 
-  def encode_token(id)
-    JWT.decode({user_id: id}, "some_secret_code")[0]["user_id"]
-  end
-
-  def get_auth_header
-    request.headers["Authorization"]
-  end
-
-  def decoded_token
-    begin
-      JWT.decode(get_auth_header, "some_secret_code")[0]["user_id"]
-    rescue
-      nil
-    end
-  end
-
-  def session_user
-    User.find_by(id: decoded_token)
-  end
-
 end
