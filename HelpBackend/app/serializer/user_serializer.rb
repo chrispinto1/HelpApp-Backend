8f86a53cb0d3c641a15aa3c757a8be59f6a4b851
davidgreenspan15 <ramigreenspan@gmail.com>
2019-07-30 15:21:03 -0400

    seed data and profile

--- HelpBackend/app/serializer/user_serializer.rb
+++ HelpBackend/app/serializer/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
- attributes :id, :username, :name, :credit_card
+ attributes :id, :username, :name, :credit_card, :password
  has_many :donations, include_nested_associations: true
  has_many :campaigns, include_nested_associations: true
 end
