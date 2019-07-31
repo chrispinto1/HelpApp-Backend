0f697ac73df8954b1bcd824ea8e911a22c171c17
davidgreenspan15 <ramigreenspan@gmail.com>
2019-07-31 10:48:01 -0400

    completed

--- HelpBackend/app/controllers/users_controller.rb
+++ HelpBackend/app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+'users_controller.rb'
 class UsersController < ApplicationController
   def index
     users = User.all
@@ -5,12 +6,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(name: params[:name],username: params[:username], password: params[:password] ,credit_card: params[:credit_card])
+    user = User.create(name: params[:name],username: params[:username], password: params[:password] ,credit_card: params[:credit_card])
 
     if user.save
       render json: user
     else
-      render json: {erros: user.errors.full_messages}
+      render json: {errors: user.errors.full_messages}
     end
   end
 
